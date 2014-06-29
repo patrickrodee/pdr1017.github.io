7c71252752979dfb8980bc845b8f6d74451b002a
pdr1017 <ninjatoad@gmail.com>
2014-06-29 03:49:38 -0700

    Site Upload 01

---
+++ js/actions.js
@@ -0,0 +1,112 @@
+var more_info_active = false;
+var sim_has_run = false;
+
+$("#more_info").click(function() {
+	if (more_info_active) {
+		$("#about_the_sim").text("");
+		more_info_active = false;
+	}
+	else {
+		$("#about_the_sim").text("This simulator takes the performance of each team in their group games into account. Each team's offensive skill is compared against their opponent's defensive skill with the thinking that a higher offensive ability will beat out a weaker defensive ability. This is 'futbol,' however, and the human factor must be taken into account. Even Messi and Ronaldo have bad days. As a result, a random 'human' factor is generated each time and applied to each teamn's rating. The final ratings, offensive and defensive, are then compared to determine which team has a greater chance of winning that match. Then, that match is simulated 10,000 times with random human factors generated each time. The result is a surprisingly accurate simulation. Case in point: Yesterday, (28 June) Brazil played Chile and Colombia played Uruguay. The sim predicted a very close victory for Brazil (the game went to overtime and was resolved on penalty kicks) and a victory for Colombia (they won 2-0). Want to see who wins? Click 'Run Simulation' and see who comes out on top.")
+		more_info_active = true;
+	}
+});
+
+function abbreviate(name) {
+	if (name == "Brazil"){return "BRA"}
+	else if (name == "Chile"){return "CHI"}
+	else if (name =="Colombia"){return "COL"}
+	else if (name =="Uruguay"){return "URU"}
+	else if (name =="France"){return "FRA"}
+	else if (name =="Nigeria"){return "NGA"}
+	else if (name =="Germany"){return "GER"}
+	else if (name =="Algeria"){return "ALG"}
+	else if (name =="Netherlands"){return "NED"}
+	else if (name =="Mexico"){return "MEX"}
+	else if (name =="Costa Rica"){return "CRC"}
+	else if (name =="Greece"){return "GRE"}
+	else if (name =="Argentina"){return "ARG"}
+	else if (name =="Switzerland"){return "SWI"}
+	else if (name =="Belgium"){return "BEL"}
+	else if (name =="USA"){return "USA"}
+};
+
+function countryCode(name) {
+	if (name == "Brazil"){return "br"}
+	else if (name == "Chile"){return "cl"}
+	else if (name =="Colombia"){return "co"}
+	else if (name =="Uruguay"){return "uy"}
+	else if (name =="France"){return "fr"}
+	else if (name =="Nigeria"){return "ng"}
+	else if (name =="Germany"){return "de"}
+	else if (name =="Algeria"){return "dz"}
+	else if (name =="Netherlands"){return "nl"}
+	else if (name =="Mexico"){return "mx"}
+	else if (name =="Costa Rica"){return "cr"}
+	else if (name =="Greece"){return "gr"}
+	else if (name =="Argentina"){return "ar"}
+	else if (name =="Switzerland"){return "ch"}
+	else if (name =="Belgium"){return "be"}
+	else if (name =="USA"){return "us"}
+};
+
+function updateNextMatch(match_number, team1, team2) {
+	var match_to_update = "#match_" + match_number.toString();
+	var first_to_update = "#first_" + match_number.toString();
+	var second_to_update = "#second_" + match_number.toString();
+	$(match_to_update).children("#team1").text(team1);
+	$(match_to_update).children("#team2").text(team2);
+	$(match_to_update).children("#teams").append(abbreviate(team1) + " vs " + abbreviate(team2));
+	$(first_to_update).attr('class', 'flag-icon flag-icon-'+countryCode(team1));
+	$(second_to_update).attr('class', 'flag-icon flag-icon-'+countryCode(team2));
+}
+
+$("#reset_sim").click(function() {
+	document.location.reload(true);
+});
+
+$("#run_sim").click(function() {
+	if (!sim_has_run) {
+		sim_has_run = true;
+		for (var i = 49; i <=56; i++) {
+			var match1_to_sim = "#match_" + i.toString();
+			i++;
+			//console.log(i);
+			var match2_to_sim = "#match_" + i.toString();
+			//console.log(match1_to_sim + ": " + $(match1_to_sim).children("#team1").text() + " vs " + $(match1_to_sim).children("#team2").text());
+			//console.log(match2_to_sim + ": " + $(match2_to_sim).children("#team1").text() + " vs " + $(match2_to_sim).children("#team2").text());
+			var outcome1 = findWinner($(match1_to_sim).children("#team1").text(), $(match1_to_sim).children("#team2").text());
+			var outcome2 = findWinner($(match2_to_sim).children("#team1").text(), $(match2_to_sim).children("#team2").text());
+			var winner1 = outcome1[0];
+			var winner2 = outcome2[0];
+			var loser1 = outcome1[1];
+			var loser2 = outcome2[1];
+			if (i == 50) {updateNextMatch(57, winner1, winner2)};
+			if (i == 52) {updateNextMatch(59, winner1, winner2)};
+			if (i == 54) {updateNextMatch(58, winner1, winner2)};
+			if (i == 56) {updateNextMatch(60, winner1, winner2)};
+		};
+		//Match 61
+		var team1_for_61 = findWinner($("#match_57").children("#team1").text(), $("#match_57").children("#team2").text());
+		var team2_for_61 = findWinner($("#match_58").children("#team1").text(), $("#match_58").children("#team2").text());
+		updateNextMatch(61, team1_for_61[0], team2_for_61[0]);
+		//Match 62
+		var team1_for_62 = findWinner($("#match_59").children("#team1").text(), $("#match_59").children("#team2").text());
+		var team2_for_62 = findWinner($("#match_60").children("#team1").text(), $("#match_60").children("#team2").text());
+		updateNextMatch(62, team1_for_62[0], team2_for_62[0]);
+		//Match 64
+		var team1_for_64 = findWinner($("#match_61").children("#team1").text(), $("#match_61").children("#team2").text());
+		var team2_for_64 = findWinner($("#match_62").children("#team1").text(), $("#match_62").children("#team2").text());
+		updateNextMatch(64, team1_for_64[0], team2_for_64[0]);
+		updateNextMatch(63, team1_for_64[1], team2_for_64[1]);
+		//Match 63
+		var first_and_second = findWinner($("#match_64").children("#team1").text(), $("#match_64").children("#team2").text());
+		var third_and_fourth = findWinner($("#match_63").children("#team1").text(), $("#match_63").children("#team2").text());
+		$("#world_cup_1st").text("Winner: " + first_and_second[0]);
+		$("#world_cup_2nd").text("Second: " + first_and_second[1]);
+		$("#world_cup_3rd").text("Third: " + third_and_fourth[0]);
+		$("#world_cup_4th").text("Fourth: " + third_and_fourth[1]);
+	};
+	return;
+});
+
---
+++ js/team_functions.js
@@ -0,0 +1,239 @@
+function accuracy(team) {
+	var i = 0;
+	while (teams[i].name != team) {
+		i++;
+	}
+	var matches = teams[i].matches;
+	var totalGoalsAttempted = 0;
+	var totalGoalsAttemptedOnTarget = 0;
+	for (var t = 0; t < matches.length; t++) {
+		totalGoalsAttempted += matches[t].goalsAttempted;
+		totalGoalsAttemptedOnTarget += matches[t].goalsAttemptedOnTarget;
+	};
+	var totalAccuracy = totalGoalsAttemptedOnTarget/totalGoalsAttempted;
+	return totalAccuracy;
+}
+
+function delivery(team) {
+	var i = 0;
+	while (teams[i].name != team) {
+		i++;
+	}
+	var matches = teams[i].matches;
+	var totalGoalsScored = 0;
+	var totalGoalsAttemptedOnTarget = 0;
+	for (var j = 0; j < matches.length; j++) {
+		totalGoalsScored += matches[j].goalsScored;
+		totalGoalsAttemptedOnTarget += matches[j].goalsAttemptedOnTarget;
+	};
+	var totalDelivery = totalGoalsScored/totalGoalsAttemptedOnTarget;
+	return totalDelivery;
+}
+
+function defense(team) {
+	var i = 0;
+	while (teams[i].name != team) {
+		i++;
+	}
+	var matches = teams[i].matches;
+	var totalOpponentAttempts = 0;
+	var totalOpponentAttemptsOnTarget = 0;
+	for (var k = 0; k < matches.length; k++) {
+		totalOpponentAttempts += matches[k].shotsAttempted;
+		totalOpponentAttemptsOnTarget += matches[k].shotsAttemptedOnTarget;
+	};
+	var totalDefense = 1 - totalOpponentAttemptsOnTarget/totalOpponentAttempts;
+	return totalDefense;
+}
+
+function goalie(team) {
+	var i = 0;
+	while (teams[i].name != team) {
+		i++;
+	}
+	var matches = teams[i].matches;
+	var totalOpponentGoals = 0;
+	var totalOpponentAttemptsOnTarget = 0;
+	for (var m = 0; m < matches.length; m++) {
+		totalOpponentGoals += matches[m].shotsAllowed;
+		totalOpponentAttemptsOnTarget += matches[m].shotsAttemptedOnTarget
+	};
+	var totalGoalie = 1 - totalOpponentGoals/totalOpponentAttemptsOnTarget;
+	return totalGoalie;
+}
+
+function possession(team) {
+	var i = 0;
+	while (teams[i].name != team) {
+		i++;
+	}
+	var matches = teams[i].matches;
+	var totalPossession = 0;
+	for (var n = 0; n < matches.length; n++) {
+		totalPossession += matches[n].possession;
+	};
+	var totalPoss = totalPossession/(n+1);
+	return totalPoss;
+}
+
+function offensivePossession(team) {
+	var i = 0;
+	while (teams[i].name != team) {
+		i++;
+	}
+	var matches = teams[i].matches;
+	var totalPossession = 0;
+	for (var n = 0; n < matches.length; n++) {
+		totalPossession += matches[n].possession;
+	};
+	return totalPossession;
+}
+
+function defensivePossession(team) {
+	var i = 0;
+	while (teams[i].name != team) {
+		i++;
+	}
+	var matches = teams[i].matches;
+	var totalPossession = 0;
+	for (var n = 0; n < matches.length; n++) {
+		totalPossession += (100-matches[n].possession);
+	};
+	return totalPossession;
+}
+
+function offensiveEffectiveness(team) {
+	var i = 0;
+	while (teams[i].name != team) {
+		i++;
+	}
+	var matches = teams[i].matches;
+	var totalPossession = offensivePossession(team);
+	var totalGoalsScored = 0;
+	for (var p = 0; p < matches.length; p++) {
+		totalGoalsScored += matches[p].goalsScored;
+	};
+	var totalOffensiveEffectiveness = totalGoalsScored/totalPossession;
+	return totalOffensiveEffectiveness;
+}
+
+function defensiveEffectiveness(team) {
+	var i = 0;
+	while (teams[i].name != team) {
+		i++;
+	}
+	var matches = teams[i].matches;
+	var totalPossession = defensivePossession(team);
+	var totalOpponentGoals = 0;
+	for (var p = 0; p < matches.length; p++) {
+		totalOpponentGoals += matches[p].shotsAllowed;
+	};
+	var totalDefensiveEffectiveness = totalOpponentGoals/totalPossession;
+	return totalDefensiveEffectiveness;
+}
+
+function compareTeamRatings(team1, team2) {
+	var team1Acc = accuracy(team1);
+	var team1Del = delivery(team1);
+	var team1Def = defense(team1);
+	var team1Goa = goalie(team1);
+	var team1Rating = (team1Acc*.4 + team1Goa*.1 + team1Def*.4 + team1Goa*.1)*100;
+	document.getElementById("team1_rating").innerHTML = team1 + "'s Rating: " + rounding(team1Rating).toString();
+	var team2Acc = accuracy(team2);
+	var team2Del = delivery(team2);
+	var team2Def = defense(team2);
+	var team2Goa = goalie(team2);
+	var team2Rating = (team2Acc*.4 + team2Goa*.1 + team2Def*.4 + team2Goa*.1)*100;
+	document.getElementById("team2_rating").innerHTML = team2 + "'s Rating: " + rounding(team2Rating).toString();
+	return;
+}
+
+function findWinner(team1, team2) {
+	// Team 1 Rating
+	var team1Acc = accuracy(team1);
+	var team1Del = delivery(team1);
+	var team1Def = defense(team1);
+	var team1Goa = goalie(team1);
+	var team1OffRating = rounding((team1Acc*.4 + team1Del*.1)*100);
+	var team1DefRating = rounding((team1Def*.4 + team1Goa*.1)*100);
+	// Team 2 Rating
+	var team2Acc = accuracy(team2);
+	var team2Del = delivery(team2);
+	var team2Def = defense(team2);
+	var team2Goa = goalie(team2);
+	var team2OffRating = rounding((team2Acc*.4 + team2Del*.1)*100);
+	var team2DefRating = rounding((team2Def*.4 + team2Goa*.1)*100);
+	// Comparison
+	var team1Wins = 0;
+	var team2Wins = 0;
+	for (var i = 0; i < 10000; i++) {
+		if ((team1OffRating/randomness(1,4) - team2DefRating/randomness(1,4)) > (team2OffRating/randomness(1,4) - team1DefRating/randomness(1,4))) {
+			team1Wins++;
+		}
+		else {
+			team2Wins++;
+		}
+	};
+	if (team1Wins > team2Wins) {
+		return [team1,team2];
+	}
+	else {
+		return [team2,team1];
+	}
+}
+
+function compareTeams(team1, team2) {
+	var team1Acc = accuracy(team1);
+	var team1Del = delivery(team1);
+	var team1Def = defense(team1);
+	var team1Goa = goalie(team1);
+	var team1OffEff = offensiveEffectiveness(team1);
+	var team1DefEff = defensiveEffectiveness(team1);
+	var team2Acc = accuracy(team2);
+	var team2Del = delivery(team2);
+	var team2Def = defense(team2);
+	var team2Goa = goalie(team2);
+	var team2OffEff = offensiveEffectiveness(team2);
+	var team2DefEff = defensiveEffectiveness(team2);
+	var team1Final = 1 + (((team1Acc*team1OffEff) - (team2Def*team2DefEff)) + ((team1Del*team1OffEff) - (team2Goa*team2DefEff)));
+	var team2Final = 1 + (((team2Acc*team2OffEff) - (team1Def*team1DefEff)) + ((team2Del*team2OffEff) - (team1Goa*team1DefEff)));
+	return [team1Final, team2Final];
+}
+
+function randomness(min, max) {
+	return Math.floor(Math.random()*(max-min+1)+min)/10;
+}
+
+function displayOutcome(team1, team2) {
+	results = compareTeams(team1, team2);
+	team1Results = results[0];
+	team2Results = results[1];
+	var output;
+	if (team1Results > team2Results) {
+		var goalDifference = Math.ceil(team1Results - team2Results);
+		output = team1 + " will win by " + goalDifference.toString();
+	}
+	else if (team1Results < team2Results) {
+		var goalDifference = Math.ceil(team2Results - team1Results);
+		output = team2 + " will win by " + goalDifference.toString();
+	}
+	document.getElementById("team1_points").innerHTML = team1 + ", " + rounding(team1Results).toString();
+	document.getElementById("team2_points").innerHTML = team2 + ", " + rounding(team2Results).toString();
+	return;
+}
+
+function rounding(value) {
+	return Math.round(value*1000)/1000;
+}
+
+function displayRatings(team) {
+	var teamAccuracy = rounding(accuracy(team));
+	var teamDelivery = rounding(delivery(team));
+	var teamDefense = rounding(defense(team));
+	var teamGoalie = rounding(goalie(team));
+	document.getElementById("accuracy_target").innerHTML = teamAccuracy;
+	document.getElementById("delivery_target").innerHTML = teamDelivery;
+	document.getElementById("defense_target").innerHTML = teamDefense;
+	document.getElementById("goalie_target").innerHTML = teamGoalie;
+	return;
+}
